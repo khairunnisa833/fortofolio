82165bb4ebb1d2b612be467c449b1cec6e6d7469
khairunnisa <nisa8334@gmail.com>
2025-09-20 12:30:47 +0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const kotak = document.querySelector(".navlist");
+function hidemenu() {
+  kotak.style.right = "-250px";
+}
+function sidemenu() {
+  kotak.style.right = "0";
+}
+
+// document.addEventListener("click", function (e) {
+//   if (!kotak.contains(e.target) && !e.target.closest(".menu")) {
+//     hidemenu();
+//   }
+// });
+let menu = document.querySelector(".menu");
+document.addEventListener("click", function (e) {
+  if (!kotak.contains(e.target) && !menu.contains(e.target)) {
+    hidemenu();
+  }
+});
